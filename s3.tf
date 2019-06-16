2508809893ad1c7d2b3241150dcde276448e2574
Colm Prunty <colmpruntysignups+git@gmail.com>
2019-06-16 14:34:00 +0100

    small change

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "super-s3-bucket" {
   bucket = "colm-terraform-test"
   acl    = "private"
 
   tags = {
-    Name        = "The worlds greatest s3 bucket"
+    Name = "The worlds greatest s3 bucket"
   }
 }
